cd35240fd8a710c5de2b213208696ee53d911f26
Melady1980 <carol.d.hernandez@outlook.com>
2022-08-03 01:05:07 +0000

    Fizz-Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers, Fizz, and Buzz replacing some numbers.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+			printf("%d ", x);
+	}
+	putchar('\n');
+}
