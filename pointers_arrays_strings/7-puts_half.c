f481f370ba0276fb69672b666b78cb113fe5d685
Melady1980 <carol.d.hernandez@outlook.com>
2022-08-10 23:13:24 +0000

    Function that prints half of a string.

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,19 +8,19 @@
 void puts_half(char *str)
 {
 	int i = 0;
-	int x = 0;
+	int n = 0;
 
 	while (str[i] != '\0')
 	{
 		i++;
 	}
-	x = i / 2;
+	n = i / 2;
 	if (i % 2 == 1)
-		x++;
-	while (str[x] != '\0')
+		n++;
+	while (str[n] != '\0')
 	{
-		_putchar(str[x]);
-		x++;
+		_putchar(str[n]);
+		n++;
 	}
 	_putchar('\n');
 }
