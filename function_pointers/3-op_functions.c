d7f039c9e43de4ca346d6e0dcbef81818040a565
Melady1980 <carol.d.hernandez@outlook.com>
2022-09-28 19:28:47 +0000

    Program that performs simple operations.

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -2,62 +2,62 @@
 #include <stdlib.h>
 #include "3-calc.h"
 /**
- * opp_add - Function that returns the sum
+ * op_add - Function that returns the sum
  * @a: number 1
  * @b: number 2
  * Return: sum
  */
 int op_add(int a, int b)
 {
-    return (a + b);
+return (a + b);
 }
 /**
- * opp_sub - Function that returns the difference of a and b
+ * op_sub - Function that returns the difference of a and b
  * @a: number 1
  * @b: number 2
  * Return: difference of a and b
  */
 int op_sub(int a, int b)
 {
-    return (a - b);
+return (a - b);
 }
 /**
- * opp_mul - Function that returns the product of a and b
+ * op_mul - Function that returns the product of a and b
  * @a: number 1
  * @b: number 2
  * Return: product of a and b
  */
 int op_mul(int a, int b)
 {
-    return (a * b);
+return (a * b);
 }
 /**
- * opp_div - Function that returns the division of a by b
+ * op_div - Function that returns the division of a by b
  * @a: number 1
  * @b: number 2
  * Return: division of a by b
  */
 int op_div(int a, int b)
 {
-    if (b == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
-    return (a / b);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a / b);
 }
 /**
- * opp_mod - Function that returns the reminder of the division of a by b
+ * op_mod - Function that returns the reminder of the division of a by b
  * @a: number 1
  * @b: number 2
  * Return: the reminder of the division of a by b
  */
 int op_mod(int a, int b)
 {
-    if (b == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
-    return (a % b);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a % b);
 }
