3251a042a54765d68250d8116f84db549f8a675b
Melady1980 <carol.d.hernandez@outlook.com>
2022-10-01 17:51:02 +0000

    Function that prints strings.

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -9,26 +9,26 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-unsigned int i;
-va_list valist;
-char *aux;
+	unsigned int i;
+	va_list valist;
+	char *aux;
 
-va_start(valist, n);
-for (i = 0; i < n; i++)
-{
-aux = va_arg(valist, char*);
-if (aux == NULL)
-printf("(nil)");
+	va_start(valist, n);
+	for (i = 0; i < n; i++)
+	{
+		aux = va_arg(valist, char*);
+		if (aux == NULL)
+			printf("(nil)");
 
-else
-{
-if (i < (n - 1) && separator != 0)
-printf("%s%s", aux, separator);
-else
-printf("%s", aux);
-}
+		else
+		{
+			if (i < (n - 1) && separator != 0)
+				printf("%s%s", aux, separator);
+			else
+				printf("%s", aux);
+		}
 
-}
-va_end(valist);
-printf("\n");
+	}
+	va_end(valist);
+	printf("\n");
 }
