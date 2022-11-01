8657e10044a7dc4902e7f6eea041345e241653c0
Melady1980 <carol.d.hernandez@outlook.com>
2022-11-01 23:17:14 +0000

    Function that creates a file.

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,18 +8,18 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, fd_write;
+	int fd, fd_write;
 
-    if (filename == NULL)
-        return (-1);
-    fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-    if (fd == -1)
-        return (-1);
-    if (text_content == NULL)
-        return (-1);
-    fd_write = write(fd, text_content, strlen(text_content));
-    close(fd);
-    if (fd_write == -1)
-        return (-1);
-    return (-1);
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+		return (1);
+	fd_write = write(fd, text_content, strlen(text_content));
+	close(fd);
+	if (fd_write == -1)
+		return (-1);
+	return (1);
 }
