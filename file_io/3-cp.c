76a96ef8dfb809e324ab6556614b2c688adf72be
Melady1980 <carol.d.hernandez@outlook.com>
2022-11-05 18:37:59 +0000

    Program that copies the content of a file to another file.

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -12,22 +12,16 @@ int main(int argc, char *argv[])
 	char buffer[1024];
 
 	if (argc != 3)
-	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
 			exit(97);
-	}
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
 				argv[1]), exit(98);
-	}
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
 			exit(99);
-	}
 	in = out = 1;
 	while (in)
 	{
